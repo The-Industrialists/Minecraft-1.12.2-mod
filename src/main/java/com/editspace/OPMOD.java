8d83ea230e8b7b9d774df3b8b892a69c9670ff4e
editspace <editspace@protonmail.com>
2024-07-18 13:31:37 -0700

    added proxys

--- src/main/java/com/editspace/OPMOD.java
+++ src/main/java/com/editspace/OPMOD.java
@@ -1,6 +1,10 @@
 package com.editspace;
 
+import com.editspace.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,11 +18,28 @@ public class OPMOD {
 	public static final String MODID = "editspace";
 	public static final String NAME = "Op mod";
 	public static final String VERSION = "0.0.1";
+
+	@Mod.Instance(MODID)
+	public static OPMOD Instance;
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+	@SidedProxy(serverSide = "com.editspace.proxy.CommonProxy", clientSide = "com.editspace.proxy.ClientProxy")
+	public static CommonProxy proxy;
+
 	
 	@Mod.EventHandler
 	public void preinit(FMLPreInitializationEvent preinit) {
 		LOGGER.info("Hello, world!");
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
 	}
 }
---
+++ src/main/java/com/editspace/proxy/CommonProxy.java
@@ -0,0 +1,4 @@
+package com.editspace.proxy;
+
+public class CommonProxy {
+}
